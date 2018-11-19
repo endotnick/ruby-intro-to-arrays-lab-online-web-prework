a1d9c9cfac846ec69a476d3c8d030a37afe9134a
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-19 03:02:53 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,27 @@ end
 def array_with_two_elements
   [1,2]
 end
+
+def first_element(arr)
+  arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  arr.shift()
+end
+
+def last_element_with_array_methods(arr)
+  arr.pop()
+end
+
+def length_of_arry(arr)
+  arr.length(0)
+end
